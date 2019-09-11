139b70de25c0e5043c431e8ce6e82313e1baa925
evansibok <evansibok@gmail.com>
2019-09-11 14:39:35 +0100

    created components and added them to parent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Header from './components/Header';
+import Main from './components/Main';
+import Section from './components/Section';
 import "./App.css";
 
-const nasaApi = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+const nasaApi = "https://api.nasa.gov/planetary/apod?api_key=HM862xmQgdGXWV6E6X37GTMWbZ4aCL9dqp8Lz5jD";
 
-function App() {
+function Gallery() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Header />
+      <Main />
+      <Section />
     </div>
   );
 }
 
-export default App;
+export default Gallery;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Main() {
+
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Main;
