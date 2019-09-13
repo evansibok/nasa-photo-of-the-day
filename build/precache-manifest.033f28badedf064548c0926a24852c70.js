fca218a070844bcd5c39bc5dd7e996b3db9d6bda
evansibok <evansibok@gmail.com>
2019-09-13 17:25:52 +0100

    added build files

---
+++ build/precache-manifest.033f28badedf064548c0926a24852c70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f547245d33d17b43b9d3717feda15dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3fea600516ec43182bc",
+    "url": "/static/css/main.14597bf1.chunk.css"
+  },
+  {
+    "revision": "44d3b18085098315d945",
+    "url": "/static/js/2.0d5d58be.chunk.js"
+  },
+  {
+    "revision": "f3fea600516ec43182bc",
+    "url": "/static/js/main.524e13d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
